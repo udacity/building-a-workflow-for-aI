7ffaf5aaa6cb59935a60d23c7b41f0b1e7f0f3d3
metin-akyol <metin.adige.akyol@gmail.com>
2024-08-23 16:25:54 -0400

    added demo 1 and 2

--- .ipynb_checkpoints/func_lib-checkpoint.py
+++ .ipynb_checkpoints/func_lib-checkpoint.py
@@ -1,7 +1,7 @@
 # Import yfinance library
 import yfinance as yf
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 def createHistPrices():
     # Define the start and end dates  
@@ -36,13 +36,16 @@ def createHistPrices():
 
     return historical_prices
 
+
+
+
 # Create a function called 'computingReturns' that takes prices and a list of integers (momentums) as an inputs
-def computingReturns(prices, list_of_momentums): 
+def computingReturns(historical_prices, list_of_momentums): 
     # Initialize the forecast horizon
     forecast_horizon = 1
     # Compute forward returns by taking percentage change of close prices
     # and shifting by the forecast horizon
-    f_returns = prices.pct_change(forecast_horizon, fill_method=None)
+    f_returns = historical_prices.pct_change(forecast_horizon, fill_method=None)
     f_returns = f_returns.shift(-forecast_horizon)
     # Convert the result to a DataFrame
     f_returns = pd.DataFrame(f_returns.unstack())
@@ -55,7 +58,7 @@ def computingReturns(prices, list_of_momentums):
     # Iterate over the list of momentum values
     for i in list_of_momentums:   
         # Compute returns for each momentum value
-        feature = prices.pct_change(i, fill_method=None)
+        feature = historical_prices.pct_change(i, fill_method=None)
         feature = pd.DataFrame(feature.unstack())
         # Name the column based on the momentum value
         name = str(i) + "_d_returns"        
@@ -70,96 +73,3 @@ def computingReturns(prices, list_of_momentums):
 
     # Return the computed total returns DataFrame
     return total_returns
-
-def compute_BM_Perf(total_returns):
-    # Compute the daily mean of all stocks. This will be our equal weighted benchmark
-    daily_mean  = pd.DataFrame(total_returns.loc[:,'F_1_d_returns'].groupby(level='Date').mean())
-    daily_mean.rename(columns={'F_1_d_returns':'SP&500'}, inplace=True)
-    
-    # Convert daily returns to cumulative returns
-    cum_returns = pd.DataFrame((daily_mean[['SP&500']]+1).cumprod())
-    
-    # Plotting the cumulative returns
-    cum_returns.plot()
-    
-    # Customizing the plot
-    plt.title('Cumulative Returns Over Time', fontsize=16, fontweight='bold')
-    plt.xlabel('Date', fontsize=14)
-    plt.ylabel('Cumulative Return', fontsize=14)
-    plt.grid(True)
-    plt.xticks(rotation=45)
-    plt.legend(title_fontsize='13', fontsize='11')
-    
-    # Display the plot
-    plt.show()
-    
-    # Calculate the number of years in the dataset
-    number_of_years = len(daily_mean) / 252  # Assuming 252 trading days in a year
-    
-    ending_value    = cum_returns['SP&500'].iloc[-1]
-    beginning_value = cum_returns['SP&500'].iloc[1]
-    
-    # Compute the Compound Annual Growth Rate (CAGR)
-    ratio = ending_value/beginning_value
-    cagr = round((ratio**(1/number_of_years)-1)*100,2)
-    print(f'The CAGR is: {cagr}%')
-    
-    # Compute the Sharpe Ratio by annualizing the daily mean and the daily std
-    average_daily_return    = daily_mean[['SP&500']].describe().iloc[1,:] * 252
-    stand_dev_dail_return   = daily_mean[['SP&500']].describe().iloc[2,:] * pow(252,1/2)
-    
-    sharpe  = average_daily_return/stand_dev_dail_return
-    
-    print(f'Sharpe Ratio of Strategy: {round(sharpe.iloc[0],2)}')
-    
-    
-    #df_daily_mean.rename(columns={target:'Strategy'},inplace=True)
-    ann_returns = (pd.DataFrame((daily_mean[['SP&500']]+1).groupby(daily_mean.index.get_level_values(0).year).cumprod())-1)*100
-    calendar_returns  = pd.DataFrame(ann_returns['SP&500'].groupby(daily_mean.index.get_level_values(0).year).last())
-    
-    calendar_returns.plot.bar(rot=30,  legend='top_left')#.opts(multi_level=False) 
-
-    return cum_returns, calendar_returns
-
-
-
-def compute_Strat_Perf(total_returns, cum_returns, calendar_returns, trading_strategy, model_name):    
-    # Apply trading strategy to each RSI value
-    total_returns['Position'] = total_returns[model_name].transform(trading_strategy)
-    # Create Returns for each Trade
-    total_returns[f'{model_name}_Return'] = total_returns['F_1_d_returns'] *  total_returns['Position'] 
-    
-    # Compute the daily mean of all stocks. This will be our equal weighted benchmark
-    daily_mean  = pd.DataFrame(total_returns.loc[:,f'{model_name}_Return'].groupby(level='Date').mean())
-    
-    # Convert daily returns to cumulative returns
-    cum_returns.loc[:,f'{model_name}_Return']  = pd.DataFrame((daily_mean[[f'{model_name}_Return']]+1).cumprod())
-    
-    # Calculate the number of years in the dataset
-    number_of_years = len(daily_mean) / 252  # Assuming 252 trading days in a year
-    
-    ending_value    = cum_returns[f'{model_name}_Return'].iloc[-1]
-    beginning_value = cum_returns[f'{model_name}_Return'].iloc[1]
-    
-    ratio = ending_value/beginning_value
-    # Compute the Compound Annual Growth Rate (CAGR)
-    cagr = round((ratio**(1/number_of_years)-1)*100,2)
-    
-    print(f'The CAGR is: {cagr}%')
-    
-    # Compute the Sharpe Ratio by annualizing the daily mean and the daily std
-    average_daily_return  = daily_mean[[f'{model_name}_Return']].describe().iloc[1,:] * 252
-    stand_dev_dail_return   = daily_mean[[f'{model_name}_Return']].describe().iloc[2,:] * pow(252,1/2)
-    
-    # Compute the Sharpe Ratio and print it out
-    sharpe  = average_daily_return/stand_dev_dail_return
-    
-    print(f'Sharpe Ratio of Strategy: {round(sharpe.iloc[0],2)}')
-    
-    ann_returns = (pd.DataFrame((daily_mean[f'{model_name}_Return']+1).groupby(daily_mean.index.get_level_values(0).year).cumprod())-1)*100
-    
-    
-    calendar_returns.loc[:,f'{model_name}_Return']  = pd.DataFrame(ann_returns[f'{model_name}_Return'].groupby(daily_mean.index.get_level_values(0).year).last())
-    
-    calendar_returns.plot.bar(rot=30,  legend='top_left')#.opts(multi_level=False) 
-    return cum_returns, calendar_returns
